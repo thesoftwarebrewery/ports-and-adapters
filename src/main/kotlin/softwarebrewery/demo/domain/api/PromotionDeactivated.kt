1cfa49a24589a8ac39081709ce77ad610babd765
rob <rob@softwarebrewery.nl>
2022-07-07 14:44:15 +0200

    remove incorrect annotation

--- src/main/kotlin/softwarebrewery/demo/domain/api/PromotionDeactivated.kt
+++ src/main/kotlin/softwarebrewery/demo/domain/api/PromotionDeactivated.kt
@@ -1,10 +1,8 @@
 package softwarebrewery.demo.domain.api
 
-import softwarebrewery.demo.domain.*
 import softwarebrewery.demo.domain.model.*
 import java.time.*
 
-@DomainEvent
 data class PromotionDeactivated(
     val publishedAt: Instant,
     val promotionId: PromotionId,
